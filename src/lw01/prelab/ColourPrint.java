b94fb31da1c114d093846036702af1f7136f44cc
Najmullah Indonesia Sudarto Ahmad <najmullah.ina@gmail.com>
2026-09-20 20:48:39 +0700

    Labwork 01 Prelab

---
+++ src/lw01/prelab/ColourPrint.java
@@ -0,0 +1,27 @@
+package lw01.prelab;
+
+public class ColourPrint extends PrintJob {
+
+    public ColourPrint(String id, int pages) {
+        super(id, pages);
+    }
+
+    @Override
+    public int calculateCharge() {
+        int pages = getPages();
+        int pageCharge;
+
+        if (pages <= 10) {
+            pageCharge = pages * 1500;
+        } else {
+            pageCharge = (10 * 1500) + ((pages - 10) * 1000);
+        }
+
+        return pageCharge + 2000; // IDR 2000 setup cost
+    }
+
+    @Override
+    public String label() {
+        return "Colour";
+    }
+}
---
+++ src/lw01/prelab/Main.java
@@ -0,0 +1,39 @@
+package lw01.prelab;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        List<PrintJob> jobs = new ArrayList<>();
+
+        try {
+            File file = new File("src/lw01/prelab/jobs.txt");
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNext()) {
+                String type = scanner.next();
+                String id = scanner.next();
+                int pages = scanner.nextInt();
+
+                if (type.equalsIgnoreCase("MONO")) {
+                    jobs.add(new MonoPrint(id, pages));
+                } else if (type.equalsIgnoreCase("COLOUR")) {
+                    jobs.add(new ColourPrint(id, pages));
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("File jobs.txt tidak ditemukan.");
+            return;
+        }
+
+        // Loop polimorfisme (tanpa instanceof / casting)
+        for (PrintJob job : jobs) {
+            System.out.println(job.summary());
+        }
+    }
+}
